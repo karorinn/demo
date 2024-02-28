50f8fb91533a4360c1d22a3ef6e5daa8a6d01de4
karorinn <2091036@gmail.com>
2024-02-28 23:02:25 +0300

    first commit

---
+++ src/layout/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return  (
+    <nav className="page-header teal darken-2">
+      <div className="container">
+        <div className="nav-wrapper">
+          <a href="!#" className="brand-logo">MovieFinder</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="sass.html">Repository</a></li>
+
+            </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export{Header};
