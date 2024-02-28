5a701de95737adc7b9ec556ee2a9f9c78a7033fe
karorinn <120996298+karorinn@users.noreply.github.com>
2024-02-28 23:47:44 +0300

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <div className="nav-wrapper">
           <a href="!#" className="brand-logo">MovieFinder</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Repository</a></li>
+            <li><a href="https://github.com/karorinn/demo">Repository</a></li>
 
             </ul>
         </div>
